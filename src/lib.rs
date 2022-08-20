e1mo/moon-phase

commit ef8b3a1bbabf43c5f90db3638fac0917b5ee3a7c

Use chrono instead of system time
This makes for more flexible usage since it's not (if I havn't
overlooked anyting in the docs, quite possible) possible to use this
with arbitrary dates.

src/lib.rs
@@ -1,4 +1,5 @@
-use std::{f64::consts::TAU, time::SystemTime};
+use std::{f64::consts::TAU};
+use chrono::{DateTime, offset::TimeZone};
 
 const MOON_SYNODIC_PERIOD: f64 = 29.530588853; // Period of moon cycle in days.
 const MOON_SYNODIC_OFFSET: f64 = 2451550.26; // Reference cycle offset in days.
@@ -54,16 +55,13 @@ pub struct MoonPhase {
     pub zodiac_name: &'static str, // Constellation
 }
 
-fn julian_date(time: SystemTime) -> f64 {
-    let secs = match time.duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(duration) => duration.as_secs_f64(),
-        Err(earlier) => -1. * earlier.duration().as_secs_f64(),
-    };
+fn julian_date<Tz: TimeZone>(time: DateTime<Tz>) -> f64 {
+    let secs = time.timestamp_micros() as f64 / 1_000.0;
     secs / 86400. + 2440587.5
 }
 
 impl MoonPhase {
-    pub fn new(time: SystemTime) -> Self {
+    pub fn new<Tz: TimeZone>(time: DateTime<Tz>) -> Self {
         let j_date = julian_date(time);
 
         // Calculate illumination (synodic) phase.

Cargo.toml
@@ -10,3 +10,4 @@ documentation = "https://docs.rs/moon-phase/0.1.0/moon_phase/"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
+chrono = "0.4"

commit a6f285a56f186f346a6837960a72bf4a1fd92731

Fix error introduced in ef8b3a1
Lesson: don't confuse microseconds with milliseconds and
*test your changes*

src/lib.rs
@@ -57,7 +57,7 @@ pub struct MoonPhase {
 }
 
 fn julian_date<Tz: TimeZone>(time: DateTime<Tz>) -> f64 {
-    let secs = time.timestamp_micros() as f64 / 1_000.0;
+    let secs = time.timestamp_micros() as f64 / 1_000_000.0;
     secs / 86400. + 2440587.5
 }
 