24f4c508b513fb113ebb5d83443e360fb72b431f
Moritz 'e1mo' Fromm <git@e1mo.de>
2022-08-21 00:50:24 +0200

    Make chrono support a feature
    For testing all features I'd reccomend something like
    <https://crates.io/crates/cargo-hack> or
    <https://crates.io/crates/cargo-all-features>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::{f64::consts::TAU};
+#[cfg(feature="chrono")]
 use chrono::{DateTime, offset::TimeZone};
+#[cfg(not(feature="chrono"))]
+use std::time::SystemTime;
 
 const MOON_SYNODIC_PERIOD: f64 = 29.530588853; // Period of moon cycle in days.
 const MOON_SYNODIC_OFFSET: f64 = 2451550.26; // Reference cycle offset in days.
@@ -43,7 +46,7 @@ const ZODIAC_ANGLES: [f64; 12] = [
     302.49, 311.72, 348.58,
 ];
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct MoonPhase {
     pub j_date: f64,
     pub phase: f64,                // 0 - 1, 0.5 = full
@@ -56,15 +59,48 @@ pub struct MoonPhase {
     pub zodiac_name: &'static str, // Constellation
 }
 
+#[cfg(feature="chrono")]
 fn julian_date<Tz: TimeZone>(time: DateTime<Tz>) -> f64 {
     let secs = time.timestamp_micros() as f64 / 1_000_000.0;
+    julian_date_from_seconds(secs)
+}
+
+#[cfg(not(feature="chrono"))]
+fn julian_date(time: SystemTime) -> f64 {
+    let secs = match time.duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(duration) => duration.as_secs_f64(),
+        Err(earlier) => -1. * earlier.duration().as_secs_f64(),
+    };
+    julian_date_from_seconds(secs)
+}
+
+fn julian_date_from_seconds(secs: f64) -> f64 {
     secs / 86400. + 2440587.5
 }
 
 impl MoonPhase {
+    #[cfg(feature="chrono")]
     pub fn new<Tz: TimeZone>(time: DateTime<Tz>) -> Self {
         let j_date = julian_date(time);
+        Self::_new(j_date)
+    }
 
+    #[cfg(not(feature="chrono"))]
+    pub fn new(time: SystemTime) -> Self {
+        let j_date = julian_date(time);
+        Self::_new(j_date)
+    }
+
+    pub fn from_secs(secs: i64) -> Self {
+        Self::from_secs_float(secs as f64)
+    }
+
+    pub fn from_secs_float(secs: f64) -> Self {
+        let j_date = julian_date_from_seconds(secs);
+        Self::_new(j_date)
+    }
+
+    fn _new(j_date: f64) -> Self {
         // Calculate illumination (synodic) phase.
         // From number of days since new moon on Julian date MOON_SYNODIC_OFFSET
         // (1815UTC January 6, 2000), determine remainder of incomplete cycle.
@@ -140,35 +176,93 @@ impl MoonPhase {
 
 #[cfg(test)]
 mod test {
+
     use super::*;
     use super::Phase::*;
+    #[cfg(feature="chrono")]
+    use chrono::prelude::*;
+
+    use pretty_assertions::{assert_eq};
+
+    #[cfg(feature="chrono")]
+    static CHRONO_TEST_CASES: [(&str, Phase); 13] = [
+        ("1999-01-02T02:49:00+00:00", Full),
+        ("1999-07-20T09:00:00+00:00", FirstQuarter),
+
+        ("2000-01-06T18:13:00+00:00", New), // Our offset is based on that
+        ("2000-01-14T13:34:00+00:00", FirstQuarter),
+        ("2000-01-21T04:40:00+00:00", Full),
+        ("2000-01-28T07:56:00+00:00", LastQuarter),
+        ("2000-12-25T17:21:00+00:00", New),
+
+        ("2022-01-02T18:33:00+00:00", New),
+        ("2022-01-15T23:49:00+00:00", WaxingGibbous),
+        ("2022-01-16T00:00:00+00:00", Full),
+        ("2022-01-17T23:48:00+00:00", Full),
+        ("2022-01-18T23:59:00+00:00", Full),
+        ("2022-01-19T16:45:00+00:00", WainingGibbous),
+    ];
 
     #[test]
+    #[cfg(feature="chrono")]
     fn phase_detection() {
         // Times taken from https://www.timeanddate.com/moon/phases/timezone/utc
-        let testcases = [
-            ("1999-01-02T02:49:00+00:00", Full),
-            ("1999-07-20T09:00:00+00:00", FirstQuarter),
-
-            ("2000-01-06T18:13:00+00:00", New), // Our offset is based on that
-            ("2000-01-14T13:34:00+00:00", FirstQuarter),
-            ("2000-01-21T04:40:00+00:00", Full),
-            ("2000-01-28T07:56:00+00:00", LastQuarter),
-            ("2000-12-25T17:21:00+00:00", New),
-
-            ("2022-01-02T18:33:00+00:00", New),
-            ("2022-01-15T23:49:00+00:00", WaxingGibbous),
-            ("2022-01-16T00:00:00+00:00", Full),
-            ("2022-01-17T23:48:00+00:00", Full),
-            ("2022-01-18T23:59:00+00:00", Full),
-            ("2022-01-19T16:45:00+00:00", WainingGibbous),
-        ];
-
-        for (time, exp) in testcases {
+        for (time, exp) in CHRONO_TEST_CASES {
             let time = DateTime::parse_from_rfc3339(time).unwrap();
             let moon_phase = MoonPhase::new(time);
-            println!("Testing {}", time);
-            assert_eq!(moon_phase.phase_name, exp);
+            assert_eq!(moon_phase.phase_name, exp, "Failed for {}", time);
         }
+    }
+
+    #[test]
+    #[cfg(feature="chrono")]
+    pub fn chrono_seconds_same() {
+        for (time, _) in CHRONO_TEST_CASES {
+            let time = DateTime::parse_from_rfc3339(time).unwrap();
+            let seconds = time.timestamp();
+            let moon_phase_datetime = MoonPhase::new(time);
+            let moon_phase_seconds = MoonPhase::from_secs(seconds);
+            assert_eq!(moon_phase_datetime, moon_phase_seconds, "Failed for DateTime: {} / Seconds: {}", time, seconds);
+        }
+    }
+
+    #[test]
+    #[cfg(not(feature="chrono"))]
+    fn phase_detection() {
+        let testcases = [
+            ( 915245340.0, Full),	            // 1999-01-02T02:49:00+00:00
+            ( 932461200.0, FirstQuarter),	    // 1999-07-20T09:00:00+00:00
+
+            ( 947182380.0, New),	            // 2000-01-06T18:13:00+00:00
+            ( 947856840.0, FirstQuarter),	    // 2000-01-14T13:34:00+00:00
+            ( 948429600.0, Full),	            // 2000-01-21T04:40:00+00:00
+            ( 949046160.0, LastQuarter),        // 2000-01-28T07:56:00+00:00
+            ( 977764860.0, New),                // 2000-12-25T17:21:00+00:00
+
+            (1641148380.0, New),                // 2022-01-02T18:33:00+00:00
+            (1642290540.0, WaxingGibbous),	    // 2022-01-15T23:49:00+00:00
+            (1642291200.0, Full),               // 2022-01-16T00:00:00+00:00
+            (1642463280.0, Full),               // 2022-01-17T23:48:00+00:00
+            (1642550340.0, Full),               // 2022-01-18T23:59:00+00:00
+            (1642610700.0, WainingGibbous),     // 2022-01-19T16:45:00+00:00
+        ];
+
+        for (secs, exp) in testcases {
+            let moon_phase = MoonPhase::from_secs_float(secs);
+            assert_eq!(moon_phase.phase_name, exp, "Failed for {}", secs);
+        }
+    }
+
+    #[test]
+    #[cfg(feature="chrono")]
+    fn test_create() {
+        MoonPhase::new(Local::now()); // Just make sure it's not crashing
+        MoonPhase::new(Utc::now()); // Just make sure it's not crashing
+    }
+
+    #[test]
+    #[cfg(not(feature="chrono"))]
+    fn test_create() {
+        MoonPhase::new(SystemTime::now()); // Just make sure it's not crashing
     }
 }
